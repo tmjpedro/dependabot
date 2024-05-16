a55cdfb87cf50a94ee4ccf7abca318d9be3eabcb
Tiago Pedro <tiago.pedro@rows.com>
2024-05-16 14:11:36 +0100

    add app

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import express from "express";
+const app = express();
+
+const port = 8080;
+const host = "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
